722f5f9642115aa60f6487c9b7ffede80b401efb
karishma1521success <105378376+karishma1521success@users.noreply.github.com>
2023-02-23 23:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // your code here
+let submit = document.getElementById('button');
+let h3 = document.getElementById('url');
+
+function url(event){
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let year = document.getElementById('year').value;
+
+    if(name != "" && year != ""){
+        h3.textContent = 'https://localhost:8080/?name=' +name+"&year="+year;
+    }else{
+        if(name != ""){
+            h3.textContent = 'https://localhost:8080/?name=' +name;
+        }else if(year != ""){
+            h3.textContent = 'https://localhost:8080/?year=' +year;
+        }else{
+            h3.textContent= 'https://localhost:8080/';
+        }
+    }
+
+}
+
+submit.addEventListener('click',url);
